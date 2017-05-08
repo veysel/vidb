0baa08859de6f104ac81aafd1775703fe2cceb23
veysel <vinanc72@gmail.com>
2017-05-09 00:53:33 +0300

    Home model changed

--- App/Model/Home/HomeModel.cpp
+++ App/Model/Home/HomeModel.cpp
@@ -1,4 +1,20 @@
 class HomeModel
 {
+  private:
+    string HomeTitle;
+
+  public:
+    void setHomeTitle(string);
+    string getHomeTitle();
 
 } HomeModel;
+
+void HomeModel::setHomeTitle(string tempTitle)
+{
+    HomeTitle = tempTitle;
+}
+
+string HomeModel::getHomeTitle()
+{
+    return HomeTitle;
+}

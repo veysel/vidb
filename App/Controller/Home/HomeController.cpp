e1e6e5e449dab6ca0955c663a847c3c4b8543bbe
veysel <vinanc72@gmail.com>
2017-05-09 00:59:01 +0300

    Home controller changed

--- App/Controller/Home/HomeController.cpp
+++ App/Controller/Home/HomeController.cpp
@@ -1,4 +1,19 @@
+#include "../../Model/Home/HomeModel.cpp"
+
 class HomeController
 {
+  public:
+    HomeConroller();
+    string getHomeTitle();
 
 } HomeController;
+
+HomeController::HomeController()
+{
+    HomeModel.setHomeTitle("Deneme");
+}
+
+string HomeController::getHomeTitle()
+{
+    return HomeModel.getHomeTitle();
+}

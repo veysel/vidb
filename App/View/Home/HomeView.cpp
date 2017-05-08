38c0b7bd5a0f0fc33420c037507e0cac03074e49
veysel <vinanc72@gmail.com>
2017-05-09 01:01:04 +0300

    Home view changed

--- App/View/Home/HomeView.cpp
+++ App/View/Home/HomeView.cpp
@@ -1,4 +1,13 @@
+#include "../../Controller/Home/HomeController.cpp"
+
 class HomeView
 {
+  public:
+    void show();
 
 } HomeView;
+
+void HomeView::show()
+{
+    cout << HomeController.getHomeTitle();
+}
